201849705090ec777354fce024acf1d5d63b2b91
InYong Jung <83904581+iysh321@users.noreply.github.com>
2025-04-02 17:51:57 +0900

    update

---
+++ inyong/05_junit/src/main/java/com/kangbroo/section03/run/Application.java
@@ -0,0 +1,9 @@
+package com.kangbroo.section03.run;
+
+import com.kangbroo.section03.view.MenuView;
+
+public class Application {
+    public static void main(String[] args) {
+        new MenuView().mainMenuView();
+    }
+}
